d08de37ed0d517a4920e56d4867957af50770a30
shìwēi nguyen <84650617+HuskyDG@users.noreply.github.com>
2023-02-02 16:40:32 +0000

    Support if f2fs /data cannot be used as upperdir

--- magisk-module/customize.sh
+++ magisk-module/customize.sh
@@ -1,5 +1,23 @@
 SKIPUNZIP=1
 
+loop_setup() {
+  unset LOOPDEV
+  local LOOP
+  local MINORX=1
+  [ -e /dev/block/loop1 ] && MINORX=$(stat -Lc '%T' /dev/block/loop1)
+  local NUM=0
+  while [ $NUM -lt 64 ]; do
+    LOOP=/dev/block/loop$NUM
+    [ -e $LOOP ] || mknod $LOOP b 7 $((NUM * MINORX))
+    if losetup $LOOP "$1" 2>/dev/null; then
+      LOOPDEV=$LOOP
+      break
+    fi
+    NUM=$((NUM + 1))
+  done
+}
+
+
 if ! $BOOTMODE; then
     abort "! Install from Recovery is not supported"
 fi
@@ -46,6 +64,22 @@ rm -rf "$test_dir"
 if $is_support; then
     ui_print "- Directly use folder in /data"
     // clone dummy
+else
+    ui_print "! Cannot directly use /data"
+    ui_print "- Create 1,5GB (enough?) loop device..."
+    rm -rf /data/adb/overlay.img
+    dd if=/dev/zero of=/data/adb/overlay.img bs=1000000 count=1500
+    /system/bin/mkfs.ext4 /data/adb/overlay.img
+    loop_setup /data/adb/overlay.img
+    if [ -z "$LOOPDEV" ]; then
+        rm -rf /data/adb/overlay.img
+        abort "! Setup failed"
+    fi
+    mkdir /data/adb/overlay
+    mount -o rw -t ext4 "$LOOPDEV" /data/adb/overlay
+fi
+
+{
     ui_print "- Clone dummy skeletion for /system ..."
     mkdir -p /data/adb/overlay/upper/system
     magisk --clone-attr /system /data/adb/overlay/upper/system
@@ -64,7 +98,11 @@ if $is_support; then
         done
     done
     unzip -oj "$ZIPFILE" "libs/$ABI/overlayfs_system" uninstall.sh post-fs-data.sh -d "$MODPATH" 1>&2
-else
-    abort "! Your data partition cannot be used as upperdir"
+}
+
+if ! $is_support; then
+    umount -l /data/adb/overlay
+    rmdir /data/adb/overlay
 fi
+
 chmod 777 "$MODPATH/overlayfs_system"
--- magisk-module/post-fs-data.sh
+++ magisk-module/post-fs-data.sh
@@ -1,6 +1,33 @@
 MODDIR="${0%/*}"
+
+loop_setup() {
+  unset LOOPDEV
+  local LOOP
+  local MINORX=1
+  [ -e /dev/block/loop1 ] && MINORX=$(stat -Lc '%T' /dev/block/loop1)
+  local NUM=0
+  while [ $NUM -lt 64 ]; do
+    LOOP=/dev/block/loop$NUM
+    [ -e $LOOP ] || mknod $LOOP b 7 $((NUM * MINORX))
+    if losetup $LOOP "$1" 2>/dev/null; then
+      LOOPDEV=$LOOP
+      break
+    fi
+    NUM=$((NUM + 1))
+  done
+}
+
+loop_setup /data/adb/overlay.img
+
 mkdir -p /mnt/overlay_system
-mount --bind "/data/adb/overlay" /mnt/overlay_system
+
+if [ ! -z "$LOOPDEV" ]; then
+    mount  -o rw -t ext4 "$LOOPDEV" /mnt/overlay_system
+elif [ -d /data/adb/overlay ]; then
+    mount --bind "/data/adb/overlay" /mnt/overlay_system
+else
+    exit
+fi
 # overlay_system <writeable-dir> <mirror>
 chmod 777 "$MODDIR/overlayfs_system"
 "$MODDIR/overlayfs_system" /mnt/overlay_system "$(magisk --path)/.magisk/mirror"
