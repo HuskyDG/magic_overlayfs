ba03b4e44f946a212dc95cb8fe2d2dffdf11f1d4
Takeda-senpai <deadboltsx303@gmail.com>
2023-04-10 18:48:45 +0700

    fix stock overlayfs

--- native/jni/main.cpp
+++ native/jni/main.cpp
@@ -321,9 +321,9 @@ int main(int argc, const char **argv) {
         return 1;
     }
     if (strcmp(argv[1], "--test") == 0) {
-    	argc--;
-    	argv++;
-    	if (argc >= 3 && strcmp(argv[1], "--check-ext4") == 0) {
+        argc--;
+        argv++;
+        if (argc >= 3 && strcmp(argv[1], "--check-ext4") == 0) {
             struct statfs stfs{};
             return (statfs(argv[2], &stfs) == 0 && stfs.f_type == EXT4_SUPER_MAGIC)?
                 0 : 1;
@@ -495,16 +495,92 @@ int main(int argc, const char **argv) {
     for (auto &mnt : mountinfo) {
         auto info = mnt.target;
         std::string tmp_mount = tmp_dir + info;
+        struct stat st;
         for (auto &s : mount_list) {
             // only care about mountpoint under overlayfs mounted subdirectories
-       	    if (!starts_with(info.data(), string(info + "/").data()))
-       	        continue;
+            if (!starts_with(info.data(), string(s + "/").data()))
+               continue;
+            char *con;
+            std::string upperdir = std::string(argv[1]) + "/upper" + info;
+            std::string workerdir = std::string(argv[1]) + "/worker" + info;
+            std::string masterdir = std::string(argv[1]) + "/master" + info;
+            if (stat(info.data(), &st) == 0 && !S_ISDIR(st.st_mode))
+                goto bind_mount;
+            {
+                char *s = strdup(info.data());
+                char *ss = s;
+                while ((ss = strchr(ss, '/')) != nullptr) {
+                    ss[0] = '\0';
+                    auto sub = std::string(argv[1]) + "/upper" + s;
+                    if (mkdir(sub.data(), 0755) == 0 && getfilecon(s, &con) >= 0) {
+                        LOGD("clone attr [%s] from [%s]\n", con, s);
+                        chown(sub.data(), getuidof(s), getgidof(s));
+                        chmod(sub.data(), getmod(s));
+                        setfilecon(sub.data(), con);
+                        freecon(con);
+                    }
+                    ss[0] = '/';
+                    ss++;
+                }
+                free(s);
+            };
+            {
+                if (mkdir(upperdir.data(), 0755) == 0 && getfilecon(info.data(), &con) >= 0) {
+                    LOGD("clone attr [%s] from [%s]\n", con, info.data());
+                    chown(upperdir.data(), getuidof(info.data()), getgidof(info.data()));
+                    chmod(upperdir.data(), getmod(info.data()));
+                    setfilecon(upperdir.data(), con);
+                    freecon(con);
+                }
+                mkdirs(workerdir.data(), 0755);
+    
+                if (!is_dir(upperdir.data()) ||
+                    !is_dir(workerdir.data())) {
+                    LOGD("setup upperdir or workdir failed!\n");
+                    CLEANUP
+                    return 1;
+                }
+            }
+            {
+                std::string opts;
+                opts += "lowerdir=";
+                if (stat(masterdir.data(), &st) == 0 && S_ISDIR(st.st_mode))
+                    opts += masterdir + ":";
+                opts += info.data();
+                opts += ",upperdir=";
+                opts += upperdir;
+                opts += ",workdir=";
+                opts += workerdir;
+                
+                // 0 - read-only
+                // 1 - read-write default
+                // 2 - read-only locked
+                
+                if (OVERLAY_MODE == 2 || mount("overlay", tmp_mount.data(), "overlay", ((OVERLAY_MODE == 1)? 0 : MS_RDONLY), opts.data())) {
+                    LOGW("fall to read-only overlayfs...\n");
+                    opts = "lowerdir=";
+                    if (stat(masterdir.data(), &st) == 0 && S_ISDIR(st.st_mode))
+                        opts += masterdir + ":";
+                    opts += upperdir;
+                    opts += ":";
+                    opts += info.data();
+                    if (mount("overlay", tmp_mount.data(), "overlay", 0, opts.data())) {
+                        LOGW("mount overlayfs failed, fall to bind mount!\n");
+                        goto bind_mount;
+                    }
+                }
+            }
+            goto mount_done;
+               
+            bind_mount:
             if (mount(info.data(), tmp_mount.data(), nullptr, MS_BIND, nullptr)) {
                 // mount fails
                 LOGE("mount failed, abort!\n");
                 CLEANUP
                 return 1;
             }
+            
+            mount_done:
             mountpoint.emplace_back(info);
             break;
         }
