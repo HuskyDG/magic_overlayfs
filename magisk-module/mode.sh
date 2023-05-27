730a0196874cd4d5f61f0fff022008db1c97b362
Takeda-senpai <deadboltsx303@gmail.com>
2023-05-27 15:54:27 +0700

    add config DO_UNMOUNT_KSU
    to unmount KernelSU overlayfs

--- magisk-module/mode.sh
+++ magisk-module/mode.sh
@@ -3,3 +3,6 @@
 # 2 - read-only locked (cannot remount as read-write)
 
 export OVERLAY_MODE=0
+
+# If you are using KernelSU, set this to true to unmount KernelSU overlayfs
+export DO_UNMOUNT_KSU=false
--- magisk-module/service.sh
+++ magisk-module/service.sh
@@ -1,5 +1,11 @@
 touch "${0%/*}/disable"
 touch /dev/.overlayfs_service_unblock
 
+. "${0%/*}/mode.sh"
+
+if [ "$DO_UNMOUNT_KSU" == "true" ]; then
+    "${0%/*}/overlayfs_system" --unmount-ksu
+fi
+
 while [ "$(getprop sys.boot_completed)" != 1 ]; do sleep 1; done
 rm -rf "${0%/*}/disable"
