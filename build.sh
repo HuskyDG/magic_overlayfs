6e95427711fed90c8cf9bca1ab15a5c33b092ddd
Takeda-senpai <deadboltsx303@gmail.com>
2023-06-13 11:54:49 +0700

    build static

--- build.sh
+++ build.sh
@@ -6,25 +6,33 @@ build_mode="${1:-release}"
 
 cd "$(dirname "$0")"
 
-plugin() {
-rm -rf "$1"
-git clone "$2" "$1"
-}
-#      <DIRECTORY>                       <PLUG-IN LINK>
-plugin ./native/jni/libcxx               http://github.com/huskydg/libcxx || exit 1
-
-pushd native
-rm -fr libs obj
-debug_mode=1
-if [[ "$build_mode" == "release" ]]; then
-    debug_mode=0
-fi
-ndk-build -j4 NDK_DEBUG=$debug_mode
-popd
+ANDROID_NDK_HOME=./android-ndk-r23b
+export PATH=${PATH}:${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 rm -rf out
 mkdir -p out
 cp -af magisk-module out
 cp -af README.md out/magisk-module
-cp -af native/libs out/magisk-module
+mkdir -p out/magisk-module/libs
+
+( cat << EOF
+arm64-v8a aarch64-linux-android31-clang++
+armeabi-v7a armv7a-linux-androideabi31-clang++
+x86 i686-linux-android31-clang++
+x86_64 x86_64-linux-android31-clang++
+EOF
+) | while read line; do
+    ARCH="$(echo $line | awk '{ print $1 }')"
+    CXX="$(echo $line | awk '{ print $2 }')"
+    if [ ! -z "$ARCH" ]; then
+        mkdir "out/magisk-module/libs/${ARCH}"
+        ${CXX} \
+    native/jni/main.cpp \
+    native/jni/logging.cpp native/jni/utils.cpp native/jni/mountinfo.cpp \
+    -static \
+    -std=c++17 \
+    -o "out/magisk-module/libs/${ARCH}/overlayfs_system"
+    fi
+done
+
 zip -r9 out/magisk-module-release.zip out/magisk-module
--- native/jni/logging.hpp
+++ native/jni/logging.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include <android/log.h>
 #include <errno.h>
 #define LOG_TAG "OverlayFS"
 
@@ -8,16 +7,14 @@ extern int log_fd;
 #define write_log(PRIO, TAG, ...) \
     { \
       char logbuffer[4098]; \
-      __android_log_print(PRIO, TAG, __VA_ARGS__); \
       snprintf(logbuffer, sizeof(logbuffer)-1, __VA_ARGS__); \
       log_to_file(log_fd, PRIO, logbuffer); \
     }
 
-#define LOGD(...) write_log(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) write_log(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) write_log(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) write_log(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) write_log(1, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) write_log(0, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) write_log(2, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) write_log(3, LOG_TAG, __VA_ARGS__)
 #define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s\n", ##args, errno, std::strerror(errno))
 
 void log_to_file(int fd, int prio, const char *log);
