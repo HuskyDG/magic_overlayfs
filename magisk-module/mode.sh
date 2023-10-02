be40602b5a6774c5b0df244083421c9119a1ee3c
Takeda-senpai <deadboltsx303@gmail.com>
2023-10-02 16:40:45 +0700

    OVERLAY_LEGACY_MOUNT=true by default

--- magisk-module/mode.sh
+++ magisk-module/mode.sh
@@ -5,5 +5,6 @@
 # You can set to 2 after modify system partititons to avoid detection
 export OVERLAY_MODE=0
 
-# Set to true to enable legacy mode that mount overlayfs on subdirectories instead of root partititons
-export OVERLAY_LEGACY_MOUNT=false
+# Set to true to mount overlayfs on subdirectories of /system /vendor /system_ext /product,...
+# instead of on /system /vendor /system_ext /product,...
+export OVERLAY_LEGACY_MOUNT=true
